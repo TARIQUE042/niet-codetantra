f5657a25c7139cea6528a144d7e7cabc4a2e66c0
Tarique Hussain <73169319+tarique4hussain@users.noreply.github.com>
2022-10-14 11:45:22 +0530

    Update Q3.py

--- Competitive-coding-2022/LAB2/Q3.py
+++ Competitive-coding-2022/LAB2/Q3.py
@@ -1,31 +1,18 @@
 import sys
 
-n = int(sys.argv[1])
-def isprm(n):
-	if (n <= 1):
-		return 0
-	if (n <= 3):
-		return 1
-	if (n % 2 == 0 or n % 3 == 0):
-		return 0
-	i=5
-	while (i*i <= n):
-		if (n % i==0 or n % (i + 2) == 0):
-			return 0
-		i = i + 6
-	return 1
-def primeSum(n):
-	count=0
-	primevector = []
-	for i in range(2, n + 1):
-		if(isprm(i) == 1):
-			primevector.append(i)
-	sum=primevector[0]
-	for i in range(1, len (primevector)):
-		sum += primevector[i]
-		if (sum > n):
-			break
-		if (isprm(sum) == 1):
-			count += 1
-	return count
-print(primeSum(n))
+s = sys.argv[1]
+l = int(sys.argv[2])
+
+def isEqualCharacters(s, l):
+	# Write code here
+	rev=""
+	for i in s:
+		rev = i+rev
+	first=s[:l]
+	last=rev[l-1::-1]
+	if first==last[::-1]:
+		return True
+	else:
+		return False
+
+print("true" if isEqualCharacters(s, l) else "false");
